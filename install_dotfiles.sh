183643740ab0de9ee41b171f2cbb6e984a2a808b
thehnm <hngnm@protonmail.com>
2018-10-22 01:57:51 +0200

    Create and remove directories when using stow

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+confarray=(conky dunst gtk-3.0 i3 polybar termite)
+for item in ${confarray[*]}
+do
+  if [ -d "$HOME/.config/$item" ]; then
+    echo $HOME/.config/$item exists.
+  else
+    mkdir -p $HOME/.config/$item
+  fi
+done
+
 stow -R config.stow
 stow -R fonts.stow
 stow -R wallpaper.stow
--- uninstall_dotfiles.sh
+++ uninstall_dotfiles.sh
@@ -2,3 +2,13 @@
 stow -D config.stow
 stow -D fonts.stow
 stow -D wallpaper.stow
+
+confarray=(conky dunst gtk-3.0 i3 polybar termite)
+for item in ${confarray[*]}
+do
+  if [ "$(ls -A $HOME/.config/$item)" ]; then
+    echo $HOME/.config/$item is not empty.
+  else
+    rm -r $HOME/.config/$item
+  fi
+done
