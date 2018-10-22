4b767fba7a205c8763ce57ac567e2ba64753ce8f
thehnm <thehnm@posteo.net>
2018-10-22 13:04:59 +0200

    Update arch_bootstrap.sh

--- arch_bootstrap/arch_bootstrap.sh
+++ arch_bootstrap/arch_bootstrap.sh
@@ -104,7 +104,7 @@ read -p "Enter desired hostname: " hostname
 echo $hostname >> /etc/hostname
 echo 127.0.0.1  localhost >> /etc/hosts
 echo ::1	localhost >> /etc/hosts
-echo 127.0.1.1	$hostname.localdomain	$hostname
+echo 127.0.1.1	$hostname.localdomain	$hostname >> /etc/hosts
 
 echo #####################################################################################################################
 
