1e97377785f67549d630516aa5c140286f370190
thehnm <thehnm@posteo.net>
2018-10-22 02:14:29 +0200

    Add echo comments to bootstrap script

--- arch_bootstrap/arch_bootstrap.sh
+++ arch_bootstrap/arch_bootstrap.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+# To assert the file a definitive location
+echo ${bold}Move arch_bootstrap.sh file to /${normal}
 mv arch_bootstrap.sh /
 
 # Set localtime
+echo ${bold}Setting localtime to Berlin${normal}
 ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
 hwclock --systohc
 
 echo #####################################################################################################################
 
 # Set up locale
+echo ${bold}Setting locale${normal}
 sed -i 's/^#de_DE/de_DE/g' /etc/locale.gen
 sed -i 's/^#en_US/en_US/g' /etc/locale.gen
 locale-gen
@@ -18,11 +25,13 @@ echo KEYMAP=de-latin1 >> /etc/vconsole.conf
 echo #####################################################################################################################
 
 # Set root password
+echo ${bold}Setting root password${normal}
 passwd
 
 echo #####################################################################################################################
 
 # Set up user
+echo ${bold}Create new user${normal}
 read -p "Enter non-root user's name " name
 useradd -m -g wheel $name
 echo Set password for $name
@@ -33,6 +42,7 @@ sed -i 's/^# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g' /etc/sudoers
 echo #####################################################################################################################
 
 # Install packages
+echo ${bold}Install packages${normal}
 curl https://raw.githubusercontent.com/thehnm/dotfiles-i3/master/arch_bootstrap/packages.txt >> packages.txt
 xargs -a packages.txt pacman --color=auto --needed -S
 mv packages.txt /
@@ -40,6 +50,7 @@ mv packages.txt /
 echo #####################################################################################################################
 
 # Install yay aur helper
+echo ${bold}Install yay AUR helper${normal}
 sudo -u $name git clone https://aur.archlinux.org/yay.git /home/$name/yay
 cd /home/$name/yay
 sudo -u $name makepkg -si
@@ -47,17 +58,20 @@ sudo -u $name makepkg -si
 echo #####################################################################################################################
 
 # Install aur packages
+echo ${bold}Install AUR packages${normal}
 sudo -u $name yay -S i3lock-color polybar lightdm-webkit-theme-aether
 
 echo #####################################################################################################################
 
 # Systemd
+echo ${bold}Enable systemd services${normal}
 systemctl enable NetworkManager
 systemctl enable lightdm
 
 echo #####################################################################################################################
 
 # Install dotfiles
+echo ${bold}Install my i3 dotfile${normal}
 sudo -u $name git clone https://thehnm@github.com/thehnm/dotfiles-i3.git /home/$name/dotfiles-i3
 cd /home/$name/dotfiles-i3/
 sudo -H -u $name bash -c 'bash install_dotfiles.sh'
@@ -65,11 +79,13 @@ sudo -H -u $name bash -c 'bash install_dotfiles.sh'
 echo #####################################################################################################################
 
 # Exec i3 in xinitrc
+echo ${bold}Set xinitrc${normal}
 echo exec i3 >> $HOME/.xinitrc
 
 echo #####################################################################################################################
 
 # Set up hostname
+echo ${bold}Set up host${normal}
 read -p "Enter desired hostname: " hostname
 echo $hostname >> /etc/hostname
 echo 127.0.0.1  localhost >> /etc/hosts
@@ -79,7 +95,8 @@ echo 127.0.1.1	$hostname.localdomain	$hostname
 echo #####################################################################################################################
 
 # Install grub
-read -p "Enter your disk label: dev/sdx? x: " label
+echo ${bold}Set up grub boot loader${normal}
+read -p "Enter the last character your disk label where Arch is installed (dev/sd_): " label
 read -p "Do you have UEFI enabled? " answer
 while true
 do
@@ -101,17 +118,20 @@ done
 echo #####################################################################################################################
 
 # Fix audio problem
+echo ${bold}Fix audio problem${normal}
 sed -i 's/^ autospawn/; autospawn/g' /etc/pulse/client.conf
 
 echo #####################################################################################################################
 
+# Install vundle
+echo ${bold}Install vundle${normal}
 cd /home/$name
 sudo -u $name git clone https://github.com/VundleVim/Vundle.vim.git /home/$name/.vim/bundle/Vundle.vim
-sudo -u $name vim +PluginInstall +qall
 
 echo #####################################################################################################################
 
+echo ${bold}Remove packages.txt and arch_bootstrap.sh${normal}
 rm /packages.txt
-rm /arch_bootstrap.txt
+rm /arch_bootstrap.sh
 
-echo -e Done. Reboot now!
+echo Done. Reboot now!
