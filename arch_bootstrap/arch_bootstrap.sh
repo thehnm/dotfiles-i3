8b74f8fa32a3e8c3e03a12632cc2faa79bfcc7fe
thehnm <thehnm@posteo.net>
2018-10-22 16:59:16 +0200

    Update arch_bootstrap behaviour

--- arch_bootstrap/arch_bootstrap.sh
+++ arch_bootstrap/arch_bootstrap.sh
@@ -7,20 +7,98 @@ NC='\033[0m'
 
 ##########################################################################################################################
 
+echodone() {
+  echo -e " ${GREEN}[Done]${NC}"
+}
+
+movescript() {
+  }
+
+setlocaltime() {
+  echo -n "${bold}Setting localtime to Berlin${normal}"
+  ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
+  hwclock --systohc
+  echodone
+}
+
+setlocale() {
+  echo -n ${bold}Setting locale${normal}
+  sed -i 's/^#de_DE/de_DE/g' /etc/locale.gen
+  sed -i 's/^#en_US/en_US/g' /etc/locale.gen
+  locale-gen
+  echo LANG=en_US.UTF-8 >> /etc/locale.conf
+  echo LANGUAGE=en_US.UTF-8 >> /etc/locale.conf
+  echo LC_ALL=de_DE.UTF-8 >> /etc/locale.conf
+  echo KEYMAP=de-latin1 >> /etc/vconsole.conf
+  echodone
+}
+
+setrootpasswd() {
+  echo -n ${bold}Setting root password${normal}
+  passwd
+  echodone
+}
+
+setupnewuser() {
+  echo ${bold}Create new user${normal}
+  read -p "Enter non-root user's name: " name
+  useradd -m -g wheel $name
+  echo Set password for $name
+  passwd $name
+  chfn $name
+  sed -i 's/^# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g' /etc/sudoers
+}
+
+installyay() {
+  echo ${bold}Install yay AUR helper${normal}
+  sudo -u $name git clone https://aur.archlinux.org/yay.git /home/$name/yay
+  cd /home/$name/yay
+  sudo -u $name makepkg -si
+  echodone
+}
+
 install() {
   echo -n "Installing $1"
   pacman --needed -S "$1" &>/dev/null
-  echo -e " ${RED}[Done]${NC}"
+  echodone
 }
 
 aurinstall() {
   echo -n "Installing $1 from AUR"
   sudo -u $name yay -S "$1" &>/dev/null
-  echo -e " ${RED}[Done]${NC}"
+  echodone
 }
 
-installlopp() {
+installloop() {
   curl https://raw.githubusercontent.com/thehnm/dotfiles-i3/master/arch_bootstrap/packages.csv >> /tmp/packages.csv
+  while true
+  do
+    read -p "Do you want i3bar or polybar?" bar
+    case $bar in
+      "i3" ) sed -i '/A,Polybar/d' /tmp/packages.csv
+             break;;
+
+      "polybar" ) sed -i '/,i3blocks/d' /tmp/packages.csv
+                  sed -i '/,i3status/d' /tmp/packages.csv
+                  break;;
+
+      * )     echo "Dude, just enter i3bar or polybar, please.";;
+    esac
+  done
+
+  read -p "Do you want to edit the list of packages to be installed? " edit
+  while true
+  do
+    case $edit in
+      [yY]* ) nano /tmp/packages.txt
+              break;;
+
+      [nN]* ) break;;
+
+      * ) break ;;
+    esac
+  done
+
   FILE=/tmp/data.csv
   IFS=,
   [ ! -f $FILE ] && { echo "$FILE file not found"; exit 99; }
@@ -28,155 +106,114 @@ installlopp() {
   do
     case $tag in
       "") install "$program" ;;
+      "G") aurinstall "$program" ;;
     esac
   done < $INPUT
 }
 
+enableservices() {
+  echo -n ${bold}Enable systemd services${normal}
+  systemctl enable NetworkManager
+  systemctl enable lightdm
+  echodone
+}
+
+installdotfile() {
+  echo -n ${bold}Install my i3 dotfile${normal}
+  sudo -u $name git clone https://thehnm@github.com/thehnm/dotfiles-i3.git /home/$name/dotfiles-i3
+  cd /home/$name/dotfiles-i3/
+  case $bar in
+    "i3" ) sudo -u $name sed -i '/polybar/d' /home/$name/dotfiles-i3/config.stow/.config/i3/config
+      sudo -u $name sed -i 's/^#1//g' /home/$name/dotfiles-i3/config.stow/.config/i3/config
+      break;;
+    "polybar" ) sudo -u $name sed -i '/,i3blocks/d' /tmp/packages.csv
+      sudo -u $name sed -i '/,i3status/d' /tmp/packages.csv
+      break;;
+  esac
+  sudo -H -u $name bash -c 'bash install_dotfiles.sh'
+  echodone
+}
+
+setxinitrc() {
+  echo -n ${bold}Set xinitrc${normal}
+  echo exec i3 >> $HOME/.xinitrc
+  echodone
+}
+
+
+setuphost() {
+  echo -n ${bold}Set up host${normal}
+  read -p "Enter desired hostname: " hostname
+  echo $hostname >> /etc/hostname
+  echo 127.0.0.1        localhost >> /etc/hosts
+  echo ::1	localhost >> /etc/hosts
+  echo 127.0.1.1	$hostname.localdomain   $hostname >> /etc/hosts
+  echodone
+}
+
+installgrub() {
+  echo -n ${bold}Set up grub boot loader${normal}
+  read -p "Enter the last character your disk label where Arch is installed (dev/sd_): " label
+  while true
+  do
+    read -p "Do you have UEFI enabled? " answer
+    case $answer in
+      [yY]* ) pacman -S grub efibootmgr
+        grub-install --target=x86_64-pc --bootloader-id=GRUB --efi-directory=/boot/efi/
+        grub-mkconfig -o /boot/grub/grub.cfg
+        break;;
+
+      [nN]* ) pacman -S grub
+        grub-install --target=i386-pc /dev/sd$label
+        grub-mkconfig -o /boot/grub/grub.cfg
+        break;;
+
+      * )     echo "Dude, just enter Y or N, please."; break ;;
+    esac
+  done
+  echodone
+}
+
 ##########################################################################################################################
 
-# To assert the file a definitive location
-echo ${bold}Move arch_bootstrap.sh file to /${normal}
-mv arch_bootstrap.sh /
+echo -n "${bold}Move $1 to /tmp/${normal}"
+mv $1 /tmp/
+echodone
 
-# Set localtime
-echo ${bold}Setting localtime to Berlin${normal}
-ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
-hwclock --systohc
+setlocaltime
 
-echo #####################################################################################################################
+setlocale
 
-# Set up locale
-echo ${bold}Setting locale${normal}
-sed -i 's/^#de_DE/de_DE/g' /etc/locale.gen
-sed -i 's/^#en_US/en_US/g' /etc/locale.gen
-locale-gen
-echo LANG=en_US.UTF-8 >> /etc/locale.conf
-echo KEYMAP=de-latin1 >> /etc/vconsole.conf
+setrootpasswd
 
-echo #####################################################################################################################
+setupnewuser
 
-# Set root password
-echo ${bold}Setting root password${normal}
-passwd
+installloop
 
-echo #####################################################################################################################
+enableservices
 
-# Set up user
-echo ${bold}Create new user${normal}
-read -p "Enter non-root user's name: " name
-useradd -m -g wheel $name
-echo Set password for $name
-passwd $name
-chfn $name
-sed -i 's/^# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g' /etc/sudoers
+installdotfile
 
-echo #####################################################################################################################
+setxinitrc
 
-# Install packages
-echo ${bold}Install packages${normal}
-curl https://raw.githubusercontent.com/thehnm/dotfiles-i3/master/arch_bootstrap/packages.txt >> packages.txt
+setuphost
 
-read -p "Do you want to edit the list of packages to be installed? " answer
-while true
-do
-  case $answer in
-   [yY]* ) nano packages.txt
-           break;;
-
-   [nN]* ) break;;
-
-   * )     echo "Dude, just enter Y or N, please."; break ;;
-  esac
-done
-
-xargs -a packages.txt pacman --color=auto --needed -S
-mv packages.txt /
-
-echo #####################################################################################################################
-
-# Install yay aur helper
-echo ${bold}Install yay AUR helper${normal}
-sudo -u $name git clone https://aur.archlinux.org/yay.git /home/$name/yay
-cd /home/$name/yay
-sudo -u $name makepkg -si
-
-echo #####################################################################################################################
-
-# Install aur packages
-echo ${bold}Install AUR packages${normal}
-sudo -u $name yay -S i3lock-color polybar lightdm-webkit-theme-aether
-
-echo #####################################################################################################################
-
-# Systemd
-echo ${bold}Enable systemd services${normal}
-systemctl enable NetworkManager
-systemctl enable lightdm
-
-echo #####################################################################################################################
-
-# Install dotfiles
-echo ${bold}Install my i3 dotfile${normal}
-sudo -u $name git clone https://thehnm@github.com/thehnm/dotfiles-i3.git /home/$name/dotfiles-i3
-cd /home/$name/dotfiles-i3/
-sudo -H -u $name bash -c 'bash install_dotfiles.sh'
-
-echo #####################################################################################################################
-
-# Exec i3 in xinitrc
-echo ${bold}Set xinitrc${normal}
-echo exec i3 >> $HOME/.xinitrc
-
-echo #####################################################################################################################
-
-# Set up hostname
-echo ${bold}Set up host${normal}
-read -p "Enter desired hostname: " hostname
-echo $hostname >> /etc/hostname
-echo 127.0.0.1  localhost >> /etc/hosts
-echo ::1	localhost >> /etc/hosts
-echo 127.0.1.1	$hostname.localdomain	$hostname >> /etc/hosts
-
-echo #####################################################################################################################
-
-# Install grub
-echo ${bold}Set up grub boot loader${normal}
-read -p "Enter the last character your disk label where Arch is installed (dev/sd_): " label
-read -p "Do you have UEFI enabled? " answer
-while true
-do
-  case $answer in
-   [yY]* ) pacman -S grub efibootmgr
-           grub-install --target=x86_64-pc --bootloader-id=GRUB --efi-directory=/boot/efi/
-           grub-mkconfig -o /boot/grub/grub.cfg
-           break;;
-
-   [nN]* ) pacman -S grub
-           grub-install --target=i386-pc /dev/sd$label
-           grub-mkconfig -o /boot/grub/grub.cfg
-           break;;
-
-   * )     echo "Dude, just enter Y or N, please."; break ;;
-  esac
-done
-
-echo #####################################################################################################################
+installgrub
 
 # Fix audio problem
-echo ${bold}Fix audio problem${normal}
+echo -n ${bold}Fix audio problem${normal}
 sed -i 's/^ autospawn/; autospawn/g' /etc/pulse/client.conf
-
-echo #####################################################################################################################
+echodone
 
 # Install vundle
-echo ${bold}Install vundle${normal}
+echo -n ${bold}Install vundle${normal}
 cd /home/$name
 sudo -u $name git clone https://github.com/VundleVim/Vundle.vim.git /home/$name/.vim/bundle/Vundle.vim
+echodone
 
-echo #####################################################################################################################
+echo -n ${bold}Remove packages.txt and arch_bootstrap.sh${normal}
+rm /tmp/packages.txt
+rm /tmp/arch_bootstrap.sh
+echodone
 
-echo ${bold}Remove packages.txt and arch_bootstrap.sh${normal}
-rm /packages.txt
-rm /arch_bootstrap.sh
-
-echo Done. Reboot now!
+echo ${bold}Done. Reboot now!${normal}
