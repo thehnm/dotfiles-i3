b1492a59049ee93f740530ef90aa7c58effba22d
thehnm <hngnm@protonmail.com>
2018-10-26 02:21:03 +0200

    Update install.sh

--- arch_bootstrap/install.sh
+++ arch_bootstrap/install.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+[ -z ${dotfilesrepo+x} ] && dotfilesrepo="https://github.com/thehnm/dotfiles-i3.git"
+
 ###############################################################################
+
+initialcheck() { pacman -S --noconfirm --needed dialog || { echo "Are you sure you're running this as the root user? Are you sure you're using an Arch-based distro? ;-) Are you sure you have an internet connection?"; exit; } ;}
+
+welcomemsg() { \
+  dialog --title "Welcome!" --msgbox "Welcome to thehnm's Arch Linux Installation Script!\\n\\nThis script will automatically install a fully-featured i3wm Arch Linux desktop, which I use as my main machine.\\n\\n-thehnm" 10 60
+}
+
+preinstallmsg() { \
+  dialog --title "Start installing the script!" --yes-label "Let's go!" --no-label "No, nevermind!" --yesno "The rest of the installation will now be totally automated, so you can sit back and relax.\\n\\nIt will take some time, but when done, you can relax even more with your complete system.\\n\\nNow just press <Let's go!> and the system will begin installation!" 13 60 || { clear; exit; }
+}
 
 setlocale() {
   answer=$(dialog --title "Locale settings" --yesno "Do you want to edit the locale.gen file?" 5 45 3>&1 1>&2 2>&3 3>&1 || exit)
@@ -36,210 +48,222 @@ settimezone() {
   hwclock --systohc
 }
 
-###############################################################################
+getuserandpass() {
+  # Prompts user for new username an password.
+  # Checks if username is valid and confirms passwd.
+  name=$(dialog --inputbox "First, please enter a name for the user account." 10 60 3>&1 1>&2 2>&3 3>&1) || exit
+  namere="^[a-z_][a-z0-9_-]*$"
+  while ! [[ "${name}" =~ ${namere} ]]; do
+  	name=$(dialog --no-cancel --inputbox "Username not valid. Give a username beginning with a letter, with only lowercase letters, - or _." 10 60 3>&1 1>&2 2>&3 3>&1)
+  done
+  pass1=$(dialog --no-cancel --insecure --passwordbox "Enter a password for that user." 10 60 3>&1 1>&2 2>&3 3>&1)
+  pass2=$(dialog --no-cancel --insecure --passwordbox "Retype password." 10 60 3>&1 1>&2 2>&3 3>&1)
+  while ! [[ ${pass1} == ${pass2} ]]; do
+ 	unset pass2
+	pass1=$(dialog --no-cancel --insecure --passwordbox "Passwords do not match.\\n\\nEnter password again." 10 60 3>&1 1>&2 2>&3 3>&1)
+	pass2=$(dialog --no-cancel --insecure --passwordbox "Retype password." 10 60 3>&1 1>&2 2>&3 3>&1)
+  done ;}
 
-bold=$(tput bold)
-normal=$(tput sgr0)
-GREEN='\033[0;32m'
-NC='\033[0m'
+usercheck() { \
+	! (id -u $name &>/dev/null) ||
+	dialog --colors --title "WARNING!" --yes-label "CONTINUE" --no-label "No wait..." --yesno "The user \`$name\` already exists on this system. This script can install for a user already existing, but it will \\Zboverwrite\\Zn any conflicting settings/dotfiles on the user account.\\n\\nThis script will \\Zbnot\\Zn overwrite your user files, documents, videos, etc., so don't worry about that, but only click <CONTINUE> if you don't mind your settings being overwritten.\\n\\nNote also that this script will change $name's password to the one you just gave." 14 70
+	}
 
-echodone() {
-  echo -e " ${GREEN}[Done]${NC}"
-}
+adduserandpass() { \
+	# Adds user `$name` with password $pass1.
+	dialog --infobox "Adding user \"$name\"..." 4 50
+	useradd -m -g wheel -s /bin/bash "$name" &>/dev/null ||
+	usermod -a -G wheel "$name" && mkdir -p /home/"$name" && chown "$name":wheel /home/"$name"
+	echo "$name:$pass1" | chpasswd
+        unset pass1 pass2 ;}
 
-setrootpasswd() {
-  echo ${bold}Setting root password${normal}
-  passwd
-  echodone
-}
-
-setupnewuser() {
-  echo ${bold}Create new user${normal}
-  read -p "Enter non-root user's name: " name
-  useradd -m -g wheel $name
-  echo Set password for $name
-  passwd $name
-  chfn $name
-  sed -i 's/^# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g' /etc/sudoers
-}
-
-install() {
-  echo -n "Installing $1"
-  pacman --noconfirm --needed -S "$1" &>/dev/null
-  echodone
-}
-
-aurinstall() {
-  echo "Installing $1 from AUR"
-  sudo -u $name yay --noconfirm -S "$1"
-  echodone
+refreshkeys() { \
+	dialog --infobox "Refreshing Arch Keyring..." 4 40
+	pacman --noconfirm -Sy archlinux-keyring &>/dev/null
 }
 
 installyay() {
-  echo ${bold}Install yay AUR helper${normal}
-  pacman --noconfirm -S git &>/dev/null
-  sudo -u $name git clone https://aur.archlinux.org/yay.git /home/$name/yay
-  cd /home/$name/yay
-  sudo -u $name makepkg --noconfirm -si
-  echodone
+        dialog --infobox "Installing \"$1\", an AUR helper..." 8 50
+        pacman --noconfirm -S git &>/dev/null
+        sudo -u $name git clone https://aur.archlinux.org/yay.git /home/$name/yay &>/dev/null
+        cd /home/$name/yay
+        sudo -u $name makepkg --noconfirm -si
+}
+
+pacmaninstall() {
+        dialog --title "Installation" --infobox "Installing \`$1\` ($n of $total). $1 $2." 5 70
+        pacman --noconfirm --needed -S "$1" &>/dev/null
+}
+
+aurinstall() {
+        dialog --title "LARBS Installation" --infobox "Installing \`$1\` ($n of $total) from the AUR. $1 $2." 5 70
+        sudo -u $name yay --noconfirm -S "$1"
+}
+
+choosebar() {
+  bar=$(dialog --title "Select bar" --radiolist "Do you want i3bar or polybar?" 10 80 17 1 i3bar on 2 polybar off 3>&2 2>&1 1>&3)
+  while true;
+  do
+    bar=$(dialog --title "Select bar" --radiolist "You must choose one!" 10 80 17 1 i3bar on 2 polybar off 3>&2 2>&1 1>&3)
+    if [ "$?" -eq "0" ]; then
+      break
+    fi
+  done
+  case $bar in
+      1 ) sed -i '/A,polybar/d' $1
+          break;;
+      2 ) sed -i '/,i3blocks/d' $1
+          sed -i '/,i3status/d' $1
+          break;;
+  esac
+}
+
+chooselauncher() {
+        launcher=$(dialog --title "Select program launcher" --radiolist "Do you want dmenu or rofi?" 10 80 17 1 dmenu on 2 rofi off 3>&2 2>&1 1>&3)
+          while true;
+          do
+            launcher=$(dialog --title "Select program launcher" --radiolist "You must choose one!" 10 80 17 1 dmenu on 2 rofi off 3>&2 2>&1 1>&3)
+            if [ "$?" -eq "0" ]; then
+              break
+            fi
+          done
+          case $launcher in
+              1 ) sed -i '/,rofi/d' $1
+                        break;;
+              2 ) sed -i '/,dmenu/d' $1
+                  break;;
+          esac
 }
 
 installloop() {
   curl https://raw.githubusercontent.com/thehnm/dotfiles-i3/master/arch_bootstrap/packages.csv >> /tmp/packages.csv
-  while true
-  do
-    read -p "Do you want i3bar or polybar? " bar
-    case $bar in
-      "i3bar" ) sed -i '/A,polybar/d' /tmp/packages.csv
-                break;;
-
-      "polybar" ) sed -i '/,i3blocks/d' /tmp/packages.csv
-                  sed -i '/,i3status/d' /tmp/packages.csv
-                  sed -i '/,dmenu/d' /tmp/packages.csv
-                  break;;
-
-      * ) echo "Dude, just enter i3bar or polybar, please.";;
-    esac
-  done
-
-  read -p "Do you want to edit the list of packages to be installed? " edit
-  while true
-  do
-    case $edit in
-      [yY]* ) nano /tmp/packages.csv
-              break;;
-
-      [nN]* ) break;;
-
-      * ) break ;;
-    esac
-  done
-
-  echo -n "Installing archlinux-keyring"
-  pacman --noconfirm -Sy archlinux-keyring &>/dev/null
-  echodone
-
-  INPUT=/tmp/packages.csv
-  IFS=,
-  [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
-  while read tag program
-  do
-    case $tag in
-      "") install "$program" ;;
-      "A") aurinstall "$program" ;;
-    esac
-  done < $INPUT
+  choosebar "/tmp/packages.csv"
+  chooselauncher "/tmp/packages.csv"
+  total=$(wc -l < /tmp/packages.csv)
+  aurinstalled=$(pacman -Qm | awk '{print $1}')
+  while IFS=, read -r tag program; do
+   n=$((n+1))
+   case "$tag" in
+    "") pacmaninstall "$program" ;;
+    "A") aurinstall "$program" ;;
+   esac
+  done < /tmp/packages.csv ;
 }
 
-enableservices() {
-  echo -n ${bold}Enable systemd services${normal}
-  systemctl enable NetworkManager &>/dev/null
-  systemctl enable lightdm &>/dev/null
-  echodone
+putgitrepo() { # Downlods a gitrepo $1 and places the files in $2 only overwriting conflicts
+	dialog --infobox "Downloading and installing config files..." 4 60
+	dir=$(mktemp -d)
+	chown -R "$name":wheel "$dir"
+	sudo -u "$name" git clone "$1" "$dir"/"$3" &>/dev/null &&
+	sudo -u "$name" mkdir -p "$2" &&
+	sudo -u "$name" cp -rT "$dir"/"$3" "$2"
 }
 
-installdotfile() {
-  echo ${bold}Install my i3 dotfile${normal}
-  sudo -u $name git clone https://thehnm@github.com/thehnm/dotfiles-i3.git /home/$name/dotfiles-i3
-  cd /home/$name/dotfiles-i3/
-  case $bar in
-    "i3bar" ) sudo -u $name sed -i '/polybar/d' /home/$name/dotfiles-i3/config.stow/.config/i3/config
-              sudo -u $name sed -i '/rofi/d' /home/$name/dotfiles-i3/config.stow/.config/i3/config
-              sudo -u $name sed -i 's/^#1//g' /home/$name/dotfiles-i3/config.stow/.config/i3/config
-              break;;
-    "polybar" ) sudo -u $name sed -i '/,i3blocks/d' /tmp/packages.csv
-                sudo -u $name sed -i '/,i3status/d' /tmp/packages.csv
-                break;;
-  esac
-  sudo -H -u $name bash -c 'bash install_dotfiles.sh'
-  echodone
-}
+serviceinit() {
+        for service in "$@"; do
+	dialog --infobox "Enabling \"$service\"..." 4 40
+	systemctl enable "$service"
+	systemctl start "$service"
+        done ;}
 
 setxinitrc() {
-  echo -n ${bold}Set xinitrc${normal}
-  echo exec i3 >> $HOME/.xinitrc
-  echodone
+	dialog --infobox "Setting xinitrc..." 4 40
+        echo exec i3 >> $HOME/.xinitrc
 }
 
+newperms() { # Set special sudoers settings for install (or after).
+  dialog --infobox "Getting rid of that retarded error beep sound..." 10 50
+  sed -i "/#SCRIPT/d" /etc/sudoers
+  echo -e "$@ #SCRIPT" >> /etc/sudoers ;}
+
+systembeepoff() {
+  dialog --infobox "Getting rid of that retarded error beep sound..." 10 50
+  rmmod pcspkr
+  echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf ;}
+
+resetpulse() { dialog --infobox "Reseting Pulseaudio..." 4 50
+	killall pulseaudio
+        sudo -n "$name" pulseaudio --start ;}
+
 setuphost() {
-  echo ${bold}Set up host${normal}
-  read -p "Enter desired hostname: " hostname
-  echo $hostname >> /etc/hostname
+  host=$(dialog --inputbox "Enter the name for your host" 10 60 3>&1 1>&2 2>&3 3>&1) || exit
+  namere="^[a-z_][a-z0-9_-]*$"
+  while ! [[ "${host}" =~ ${namere} ]]; do
+  	name=$(dialog --no-cancel --inputbox "Hostname not valid. Give a hostname beginning with a letter, with only lowercase letters, - or _." 10 60 3>&1 1>&2 2>&3 3>&1)
+  done
+
+  dialog --infobox "Setting hostname..." 4 50
+  echo $host>> /etc/hostname
   echo 127.0.0.1        localhost >> /etc/hosts
   echo ::1	localhost >> /etc/hosts
-  echo 127.0.1.1	$hostname.localdomain   $hostname >> /etc/hosts
-  echodone
+  echo 127.0.1.1	$host.localdomain   $host >> /etc/hosts
 }
 
 installgrub() {
-  echo ${bold}Set up grub boot loader${normal}
-  read -p "Enter the last character your disk label where Arch is installed (dev/sd_): " label
-  while true
-  do
-    read -p "Do you have UEFI enabled? " answer
-    case $answer in
-      [yY]* ) echo -n "Installing grub"
-              pacman --noconfirm -S grub efibootmgr
-              grub-install --target=x86_64-pc --bootloader-id=GRUB --efi-directory=/boot/efi/
-              grub-mkconfig -o /boot/grub/grub.cfg
-              echodone
-              break;;
-
-      [nN]* ) echo -n "Installing grub"
-              pacman --noconfirm -S grub
-              grub-install --target=i386-pc /dev/sd$label
-              grub-mkconfig -o /boot/grub/grub.cfg
-              echodone
-              break;;
-
-      * ) echo "Dude, just enter Y or N, please.";;
-    esac
-  done
-  echodone
+  choice=$(dialog --yesno "Do you install this on a UEFI machine?" 10 80 3>&2 2>&1 1>&3)
+  case $? in
+      0 ) dialog --infobox "Installing grub for UEFI..." 4 50
+          pacman --noconfirm -S grub efibootmgr &>/dev/null
+          grub-install --target=x86_64-pc --bootloader-id=GRUB --efi-directory=/boot/efi/ &>/dev/null
+          grub-mkconfig -o /boot/grub/grub.cfg &>/dev/null
+          break;;
+      1 ) label=$(dialog --inputbox "Enter the disk where your install Arch Linux e.g. /dev/sda" 10 60 3>&1 1>&2 2>&3 3>&1) || exit
+          pacman --noconfirm -S grub
+          grub-install --target=i386-pc $label
+          grub-mkconfig -o /boot/grub/grub.cfg
+          break;;
+  esac
 }
+
 
 ##########################################################################################################################
 
-echo -n "${bold}Move arch_bootstrap.sh to /tmp/${normal}"
-mv arch_bootstrap.sh /tmp/
-echodone
+mv arch_bootstrap.sh /tmp/arch_bootstrap.sh
 
-setlocaltime
+# Check if user is root on Arch distro. Install dialog.
+initialcheck
 
-setlocale
+# Welcome user.
+welcomemsg
 
-setrootpasswd
+# Get and verify username and password.
+getuserandpass
 
-setupnewuser
+# Give warning if user already exists.
+usercheck || { clear; exit; }
+
+setuphost
+
+# Last chance for user to back out before install.
+preinstallmsg || { clear; exit; }
+
+adduserandpass
+
+refreshkeys
+
+newperms "%wheel ALL=(ALL) NOPASSWD: ALL"
 
 installyay
 
 installloop
 
-enableservices
+# Install the dotfiles in the user's home directory
+putgitrepo "$dotfilesrepo" "/home/$name" "dotfiles-i3"
 
-installdotfile
+putgitrepo "https://github.com/VundleVim/Vundle.vim.git" "home/$name/.vim/bundle/" "Vundle.vim"
 
-setxinitrc
+# Pulseaudio, if/when initially installed, often needs a restart to work immediately.
+[[ -f /usr/bin/pulseaudio ]] && resetpulse
 
-setuphost
+serviceinit NetworkManager
 
-installgrub
+systembeepoff
+
+newperms "%wheel ALL=(ALL) ALL\\n%wheel ALL=(ALL) NOPASSWD: /usr/bin/shutdown,/usr/bin/reboot,/usr/bin/systemctl suspend,/usr/bin/wifi-menu,/usr/bin/mount,/usr/bin/umount,/usr/bin/pacman -Syu,/usr/bin/pacman -Syyu,/usr/bin/packer -Syu,/usr/bin/packer -Syyu,/usr/bin/systemctl restart NetworkManager,/usr/bin/rc-service NetworkManager restart,/usr/bin/pacman -Syyu --noconfirm,/usr/bin/loadkeys,/usr/bin/yay"
+
+sed -i "s/^#Color/Color/g" /etc/pacman.conf
 
 # Fix audio problem
-echo -n ${bold}Fix audio problem${normal}
 sed -i 's/^ autospawn/; autospawn/g' /etc/pulse/client.conf
-echodone
 
-# Install vundle
-echo -n ${bold}Install vundle${normal}
-cd /home/$name/
-sudo -u $name git clone https://github.com/VundleVim/Vundle.vim.git /home/$name/.vim/bundle/Vundle.vim
-echodone
-
-echo -n ${bold}Remove packages.csv and arch_bootstrap.sh${normal}
-rm /tmp/packages.csv
-rm /tmp/arch_bootstrap.sh
-echodone
-
-echo ${bold}Done. Reboot now!${normal}
+installgrub
