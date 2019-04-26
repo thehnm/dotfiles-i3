7e6867b96b434b7154a41e755a9fabfeb01c4ff4
thehnm <thehnm@posteo.net>
2019-04-26 14:09:48 +0200

    Remove ncmpcpp folder

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -4,7 +4,6 @@ mkdir -p $HOME/.config/dunst
 mkdir -p $HOME/.config/mpd
 mkdir -p $HOME/.config/mpd/playlists
 mkdir -p $HOME/.config/gtk-3.0
-mkdir -p $HOME/.config/ncmpcpp
 mkdir -p $HOME/.config/termite
 mkdir -p $HOME/.config/nvim
 mkdir -p $HOME/.themes
