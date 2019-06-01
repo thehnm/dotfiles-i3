0220afb50419cc913eb40bb4a16e9bc2afd5e70d
thehnm <thehnm@posteo.net>
2019-06-01 20:17:01 +0200

    Change install file

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-mkdir -p $HOME/.config/i3
 mkdir -p $HOME/.config/dunst
 mkdir -p $HOME/.config/mpd
 mkdir -p $HOME/.config/mpd/playlists
 mkdir -p $HOME/.config/ncmpcpp
 mkdir -p $HOME/.config/nvim
 mkdir -p $HOME/.config/newsboat
-mkdir -p $HOME/.config/termite
 mkdir -p $HOME/.config/gtk-3.0
 mkdir -p $HOME/.config/calcurse
 mkdir -p $HOME/pic
