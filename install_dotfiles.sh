d03b31ea101fd1510d812ab20c6597255989e669
thehnm <thehnm@posteo.net>
2019-09-21 01:09:43 +0200

    Remove calcurse and newsboat

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -5,9 +5,6 @@ mkdir -p $HOME/.config/mpd
 mkdir -p $HOME/.config/mpd/playlists
 mkdir -p $HOME/.config/ncmpcpp
 mkdir -p $HOME/.config/nvim
-mkdir -p $HOME/.config/newsboat
-mkdir -p $HOME/.config/gtk-3.0
-mkdir -p $HOME/.config/calcurse
 mkdir -p $HOME/.local/bin
 mkdir -p $HOME/.local/share/fonts
 mkdir -p $HOME/pic
