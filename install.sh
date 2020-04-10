d40b93ae3bac3b518365506f2fc7e17d8fd37dd3
thehnm <thehnm@posteo.net>
2020-04-10 11:54:15 +0200

    Remove firefox entry in install.sh

--- install.sh
+++ install.sh
@@ -9,13 +9,11 @@ mkdir -p ~/.local/bin
 mkdir -p ~/.local/share/fonts
 mkdir -p ~/pics
 mkdir -p ~/dl
-mkdir -p ~/.mozilla/firefox/user.default-release
 
 stow -R config.stow
 stow -R shell.stow
 stow -R sh.stow
 stow -R zsh.stow
-stow -R firefox.stow
 
 if [ -f /usr/bin/openbox ]; then
     stow -R openbox.stow
